7c4a1ccdd8aa09da35357959c6dc94e526be0ec1
Nguyễn Duy <64348125+phanmemkhoinghiep@users.noreply.github.com>
2022-02-22 17:01:19 +0700

    Update start.py

--- src/start.py
+++ src/start.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from flask import Flask, request
-from flask import render_template
-import json
-import datetime
-import os
-from termcolor import colored
+#-*-coding:gb2312-*-
+from flask import Flask, request, jsonify, render_template, render_template_string
 import time
+# import imp
+import sys
+import json
 import tts
-
+import main
 app = Flask(__name__)
 
+
 @app.route('/')
-def my_form():
-    return render_template('my-form.html')
+
+def text_input():
+    return render_template('input.html')
 
 @app.route('/', methods=['POST'])
 def my_form_post():
@@ -25,24 +25,24 @@ def my_form_post():
         speak_result = 'Not OK'
     except:
         speak_result = 'Not OK'
-    return render_template('my-form.html')
+    return render_template('input.html')
 
+        
+@app.route('/api', methods=['GET', 'POST'])
 
-@app.route('/webhook', methods=['POST'])
-def speaking():
-    """
-    Send a POST request to localhost:5000/webhook with a JSON body with a "p" key
-    to print that message in the server console.
-    """
-    payload = request.get_json()
-    text=payload['data']
-    speak_result =''
-    try:
-        tts.tts_vietnamese(True,text)
-        speak_result = 'Not OK'
-    except:
-        speak_result = 'Not OK'
-    return speak_result
+def api_input():
+    if request.method == 'GET':
+        return jsonify({'message' : 'Realtime Speaker API v1.0'})          
+    if request.method == 'POST':        
+        payload = request.get_json()
+        text=payload['data']
+        speak_result =''
+        try:
+            tts.main(True,text)
+            speak_result = 'Not OK'
+        except:
+            speak_result = 'Not OK'
+        return speak_result
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True, use_reloader=True, host='0.0.0.0', port=5000)
